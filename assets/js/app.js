03b6abf5c3da0548dd84a61354716c366581eb83
sebahernandez <sebaprogramer@gmail.com>
2024-02-15 20:49:36 -0300

    correccion de errores

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,14 @@ const apiUrl = "https://mindicador.cl/api";
 const getIndicador = async () => {
   try {
     const response = await fetch(`${apiUrl}`);
+    if (!response.ok) {
+      throw new Error(`Error HTTP: ${response.status}`);
+    }
     const data = await response.json();
     return data;
   } catch (error) {
     console.error(error);
+    throw new Error("No se pudo obtener el valor del indicador");
   }
 };
 
